ec27b8899b79ba10eb65ba235d1087674a54f32d
unknown <7221798@gmail.com>
2019-05-18 21:49:23 +0300

    web server and templates

--- src/main.go
+++ src/main.go
@@ -1,9 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"html/template"
+	"log"
+	"net/http"
+	"path"
+)
 
-// this is a comment
+var (
+	// компилируем шаблоны, если не удалось, то выходим
+	template1 = template.Must(template.ParseFiles(path.Join("templates", "index.html")))
+)
+/*
+type Index struct{
+	Title string
+}
+*/
+
 
 func main() {
-    fmt.Println("Hello World")
+
+	// для отдачи сервером статичных файлов из папки static
+	fs := http.FileServer(http.Dir("./templates"))
+	http.Handle("/templates/", http.StripPrefix("/templates/", fs))
+	http.HandleFunc("/", postHandler)
+	log.Println("Listening...")
+	http.ListenAndServe(":3000", nil)
+}
+
+type ViewData struct{
+    Title string
+}
+
+func postHandler(w http.ResponseWriter, r *http.Request) {
+
+	data := ViewData{
+			 Title : "Hello World",
+		 }
+
+	// обработчик запросов
+	if err := template1.Execute(w, data); err != nil {
+		log.Println(err.Error())
+		http.Error(w, http.StatusText(500), 500)
+	} else {
+		log.Println("response sended")
+	}
 }
